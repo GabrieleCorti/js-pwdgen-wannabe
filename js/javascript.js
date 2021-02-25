d56e6f87449e475128c19c59c655b802b7b3566d
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-02-25 16:48:12 +0100

    esercizio

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+alert("Ciao, immagino che tu sia qui per ottenere una Pasword. Bè... sei nel posto giusto");
+var nome = prompt("Come ti chiami?");
+var cognome = prompt("Qual'è il tuo cognome");
+var colore = prompt("Dicci quale è il tuo Colore Preferito");
+
+document.getElementById('tua_pasword').innerHTML = nome+cognome+colore +"21"
